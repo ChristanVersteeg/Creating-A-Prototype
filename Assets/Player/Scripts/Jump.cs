b10cbbd8fd07af89b781d5ef733b83fc5598a80c
Wumpie7 <chrisjaver@gmail.com>
2024-05-29 02:22:42 +0200

    Added Materials

---
+++ Assets/Player/Scripts/Jump.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpStrength = 2;
+    public event System.Action Jumped;
+
+    [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
+    private GroundCheck groundCheck;
+
+    private void Reset()
+    {
+        // Try to get groundCheck.
+        groundCheck = GetComponentInChildren<GroundCheck>();
+    }
+
+    private void Awake()
+    {
+        // Get rigidbody.
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void LateUpdate()
+    {
+        // Jump when the Jump button is pressed and we are on the ground.
+        if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded))
+        {
+            rb.AddForce(100 * jumpStrength * Vector3.up);
+            Jumped?.Invoke();
+        }
+    }
+}
