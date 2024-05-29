734865f6f89d3c130d42c7fb1700bbc6fc11c014
Wumpie7 <chrisjaver@gmail.com>
2024-05-29 02:16:02 +0200

    Added Player

---
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+    public KeyCode runningKey = KeyCode.LeftShift;
+    private Rigidbody rb;
+    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public List<System.Func<float>> speedOverrides = new();
+
+    private void Awake()
+    {
+        // Get the rigidbody on this.
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Update IsRunning from input.
+        IsRunning = canRun && Input.GetKey(runningKey);
+
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        if (speedOverrides.Count > 0)
+        {
+            targetMovingSpeed = speedOverrides[^1]();
+        }
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity = new(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+        // Apply movement.
+        rb.velocity = transform.rotation * new Vector3(targetVelocity.x, rb.velocity.y, targetVelocity.y);
+    }
+}
---
+++ Assets/Mini First Person Controller/Scripts/GroundCheck.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class GroundCheck : MonoBehaviour
+{
+    [Tooltip("Maximum distance from the ground.")]
+    public float distanceThreshold = .15f;
+
+    [Tooltip("Whether this transform is grounded now.")]
+    public bool isGrounded = true;
+    /// <summary>
+    /// Called when the ground is touched again.
+    /// </summary>
+    public event System.Action Grounded;
+
+    private const float originOffset = .001f;
+
+    private Vector3 RaycastOrigin => transform.position + Vector3.up * originOffset;
+
+    private float RaycastDistance => distanceThreshold + originOffset;
+
+    private void LateUpdate()
+    {
+        // Check if we are grounded now.
+        bool isGroundedNow = Physics.Raycast(RaycastOrigin, Vector3.down, distanceThreshold * 2);
+
+        // Call event if we were in the air and we are now touching the ground.
+        if (isGroundedNow && !isGrounded)
+        {
+            Grounded?.Invoke();
+        }
+
+        // Update isGrounded.
+        isGrounded = isGroundedNow;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw a line in the Editor to show whether we are touching the ground.
+        Debug.DrawLine(RaycastOrigin, RaycastOrigin + Vector3.down * RaycastDistance, isGrounded ? Color.white : Color.red);
+    }
+}
