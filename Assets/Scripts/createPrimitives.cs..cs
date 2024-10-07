1ace0bb68ec572beaa20ed997b8b0a84be553575
Jaimielol <jaimieglennlolparty@gmail.com>
2024-10-07 17:10:51 +0200

    minecraft STEVEEEEEEEE

---
+++ Assets/Scripts/createPrimitives.cs..cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createPrimitives : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // making minecraft steve       
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
