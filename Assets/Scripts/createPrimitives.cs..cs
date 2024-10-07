38aeb5b67b3ac2833ab96465eee1ea10fc6ae739
Jaimielol <jaimieglennlolparty@gmail.com>
2024-10-07 17:36:22 +0200

    STEVE WITHOUT EXPLOSION

--- Assets/Scripts/createPrimitives.cs..cs
+++ Assets/Scripts/createPrimitives.cs..cs
@@ -4,23 +4,10 @@ using UnityEngine;
 
 public class createPrimitives : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject steve;
     void Start()
     {
-        // making minecraft steve       
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        Instantiate(steve);
 
     }
 }
